1f1dc41509c2ee61f35b39558ef41d85f599140e
Mike McClannahan <mam8cc@gmail.com>
2013-09-25 14:59:18 -0500

    Added document saving.

--- api.rb
+++ api.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'mongoid'
 
+#Allows for testing from a Chrome Extension HTTP
+set :protection, :origin_whitelist => ['chrome-extension://hgmloofddffdnphfgcellkdfbfbjeloo']
+
 get '/' do
   "Hello, world"
 end
@@ -9,6 +12,16 @@ post '/user/' do
 	@username = params[:username]
 	@password = params[:password]
 
-	puts @username
-	puts @password
+	Users.create(
+		username: @username,
+		password: @password
+	)
+
+	"#{@username}, #{@password}"
+end
+
+class User 
+	include Mongoid::Document
+	field :username, type: String
+	field :password, type: String
 end
