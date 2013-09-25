3d58fc925135b3660d66e846ee2277658192b56a
Mike McClannahan <mam8cc@gmail.com>
2013-09-25 12:29:36 -0500

    added mongo config and user create route

--- api.rb
+++ api.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
+require 'mongoid'
+require 'bson_ext'
 
 get '/' do
   "Hello, world"
+end
+
+post '/user/' do
+	@username = params[:username]
+	@password = params[:password]
+
+	puts @username
+	puts @password
 end
