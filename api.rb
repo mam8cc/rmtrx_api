dc4c4717e4fc1d6044c032a7f209aae1e12696d4
Mike McClannahan <mam8cc@gmail.com>
2013-09-29 20:49:41 -0500

    add debugging to auth rout' :

--- api.rb
+++ api.rb
@@ -38,11 +38,32 @@ post '/user' do
 end
 
 post '/authenticate' do
+
+	request.body              # request body sent by the client (see below)
+    request.scheme            # "http"
+    request.script_name       # "/example"
+    request.path_info         # "/foo"
+    request.port              # 80
+    request.request_method    # "GET"
+    request.query_string      # ""
+    request.content_length    # length of request.body
+    request.media_type        # media type of request.body
+    request.host              # "example.com"
+    request.get?              # true (similar methods for other verbs)
+    request.form_data?        # false
+    request["SOME_HEADER"]    # value of SOME_HEADER header
+    request.referer           # the referer of the client or '/'
+    request.user_agent        # user agent (used by :agent condition)
+    request.cookies           # hash of browser cookies
+    request.xhr?              # is this an ajax request?
+    request.url               # "http://example.com/example/foo"
+    request.path              # "/example/foo"
+    request.ip                # client IP address
+    request.secure?           # false
+    request.env 
+
 	@username = params[:username]
 	@password = params[:password]
-
-	puts @username
-	puts @password
 
 	user = User.where(username: @username).first
 
