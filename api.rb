3a58373d21b0cbe72397a534da6fa9ce0e65336c
Mike McClannahan <mam8cc@gmail.com>
2013-09-29 21:14:09 -0500

    Trying some other shit

--- api.rb
+++ api.rb
@@ -39,41 +39,20 @@ end
 
 post '/authenticate' do
 
-	puts request.body              # request body sent by the client (see below)
-    puts request.scheme            # "http"
-    puts request.script_name       # "/example"
-    puts request.path_info         # "/foo"
-    puts request.port              # 80
-    puts request.request_method    # "GET"
-    puts request.query_string      # ""
-    puts request.content_length    # length of request.body
-    puts request.media_type        # media type of request.body
-    puts request.host              # "example.com"
-    puts request.get?              # true (similar methods for other verbs)
-    puts request.form_data?        # false
-    puts request["SOME_HEADER"]    # value of SOME_HEADER header
-    puts request.referer           # the referer of the client or '/'
-    puts request.user_agent        # user agent (used by :agent condition)
-    puts request.cookies           # hash of browser cookies
-    puts request.xhr?              # is this an ajax request?
-    puts request.url               # "http://example.com/example/foo"
-    puts request.path              # "/example/foo"
-    puts request.ip                # client IP address
-    puts request.secure?           # false
-    puts request.env 
+	params.inspect
 
-	@username = params[:username]
-	@password = params[:password]
+	# @username = params[:username]
+	# @password = params[:password]
 
-	user = User.where(username: @username).first
+	# user = User.where(username: @username).first
 
-	if user.password == @password
-		key = auth.createKey()
-		payload = [key]
-		return payload.to_json
-	else
-		"Whomp whomp."
-	end
+	# if user.password == @password
+	# 	key = auth.createKey()
+	# 	payload = [key]
+	# 	return payload.to_json
+	# else
+	# 	"Whomp whomp."
+	# end
 end
 
 get '/validkey' do
